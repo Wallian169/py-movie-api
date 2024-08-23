c8293567a544ae90adf504a272da3529b2eba026
Vadym Vyshnevskyi <wallian169@gmail.com>
2024-08-23 17:44:33 +0300

    changed model Movie description field

--- cinema/models.py
+++ cinema/models.py
@@ -3,8 +3,8 @@ from django.db import models
 
 class Movie(models.Model):
     title = models.CharField(max_length=100)
-    description = models.TextField(
-        max_length=1000,
+    description = models.CharField(
+        max_length=255,
     )
     duration = models.PositiveSmallIntegerField()
 
