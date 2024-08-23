f7a48bdc8ecf8d89ff0ac7c7612f87e8fabaaec8
Vadym Vyshnevskyi <wallian169@gmail.com>
2024-08-23 22:10:44 +0300

    implemented cinema_detail_view that allows to get single object, update or delete it depending on request method used

--- cinema/urls.py
+++ cinema/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 
-from cinema.views import cinema_list_view
+from cinema.views import cinema_list_view, cinema_detail_view
 
 urlpatterns = [
     path("movies/", cinema_list_view, name="movies"),
-    # path("movies/<int:pk>/", cinema_list_view, name="movie-detail"),
+    path("movies/<int:pk>/", cinema_detail_view, name="movie-detail"),
 ]
 
 app_name = "cinema"
--- cinema/views.py
+++ cinema/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpRequest, HttpResponse
 from rest_framework import status
 from rest_framework.decorators import api_view
+from rest_framework.generics import get_object_or_404
 from rest_framework.response import Response
 
 from cinema.models import Movie
@@ -19,3 +20,24 @@ def cinema_list_view(request: HttpRequest) -> HttpResponse:
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(["GET", "PUT", "DELETE"])
+def cinema_detail_view(request: HttpRequest, pk: int) -> HttpResponse:
+    movie = get_object_or_404(Movie, pk=pk)
+    if request.method == "GET":
+        serializer = MovieSerializer(movie)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    if request.method == "PUT":
+        serializer = MovieSerializer(instance=Movie, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        else:
+            return Response(
+                serializer.errors,
+                status=status.HTTP_400_BAD_REQUEST
+            )
+    else:
+        movie.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
