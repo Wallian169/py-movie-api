217bb9e1b0cc4ad50acdca550507b0a1340e3e81
Vadym Vyshnevskyi <wallian169@gmail.com>
2024-08-23 21:26:36 +0300

    implemented cinema_list_view
    that view can return all instances of movie or create new one depending on request method used

--- cinema/views.py
+++ cinema/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-# Create your views here.
+from cinema.models import Movie
+from cinema.serializers import MovieSerializer
+
+
+@api_view(["GET", "POST"])
+def cinema_list_view(request: HttpRequest) -> HttpResponse:
+    if request.method == "GET":
+        movies = Movie.objects.all()
+        serializer = MovieSerializer(movies, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    else:
+        serializer = MovieSerializer(data=request.data, many=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
